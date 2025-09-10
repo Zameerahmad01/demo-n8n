a93e39e244351e41be86946e2af85a2fba018357
Zameerahmad01 <zameerahmad4877@gmail.com>
2025-09-10 21:10:16 +0500

    add console

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
 
 const app = express();
 app.use(express.json());
 
-app.get('/health', (_req: Request, res: Response) => {
-  res.status(400).json({ status: 'ok' })
+app.get("/health", (_req: Request, res: Response) => {
+  res.status(400).json({ status: "ok" });
+  console.log("hello");
 });
 
-app.get('/greet/:name', (req: Request, res: Response) => {
+app.get("/greet/:name", (req: Request, res: Response) => {
   const { name } = req.params;
   res.json({ message: `Hello, ${name}!` });
 });
